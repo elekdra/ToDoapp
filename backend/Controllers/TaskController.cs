b6ddbf216bee6f8d7e3927b41903e2a00b4ce32d
elekdra <swalitm@gmail.com>
2021-12-10 18:16:21 +0530

    modified

--- backend/Controllers/TaskController.cs
+++ backend/Controllers/TaskController.cs
@@ -23,63 +23,112 @@ namespace backend.Controllers
 
 
         [HttpPut]
-        [Route("filesave")]
-        public IActionResult PutFileNames([FromBody] FileModel model)
-        {
-            Console.WriteLine("check");
-            string webRootPath = environment.WebRootPath;
-            string filesPath = Path.Combine(webRootPath, "files");
-            string json = JsonFilePath();
-            var jsonString = System.IO.File.ReadAllText(json);
-            object jsonObject = JsonConvert.DeserializeObject(jsonString);
-            var list = JsonConvert.DeserializeObject<List<FileModel>>(jsonString);
-            list.Add(model);
-            var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
-            System.IO.File.WriteAllText(json, convertedJson);
-            return Ok();
-        }
-
-        [HttpPut]
         [Route("filesave2")]
         public IActionResult PutFileNames2([FromBody] FileModel model)
         {
-            Console.WriteLine("check");
+            Console.WriteLine("hi enterd");
+
             string webRootPath = environment.WebRootPath;
             string filesPath = Path.Combine(webRootPath, "files");
             var currentDirectory = System.IO.Directory.GetCurrentDirectory();
-            string jsonPath = currentDirectory + @"\wwwroot\ConfigData\inprogressdata.json";
-            string json = jsonPath;
-            var jsonString = System.IO.File.ReadAllText(json);
-            object jsonObject = JsonConvert.DeserializeObject(jsonString);
-            var list = JsonConvert.DeserializeObject<List<FileModel>>(jsonString);
-            list.Add(model);
-            var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
-            System.IO.File.WriteAllText(json, convertedJson);
+            string jsonPath = "";
+            string[] state = model.Mode.Split("|");
+            Console.WriteLine(state[0]);
+            if (state[0] == "edit")
+            {
+
+                if (state[1] == "Open")
+                {
+                    Console.WriteLine("EDIT IN OPEN");
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\new.json";
+
+                }
+                else if (state[1] == "In Progress")
+                {
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\inprogressdata.json";
+                    Console.WriteLine("EDIT IN INPROGRESS");
+                }
+                else
+                {
+                    Console.WriteLine("EDIT IN FINISHED");
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\finisheddata.json";
+                }
+                var jsonString = System.IO.File.ReadAllText(jsonPath);
+
+                Console.WriteLine("--------------------");
+                object jsonObject = JsonConvert.DeserializeObject(jsonString);
+                var list = JsonConvert.DeserializeObject<List<FileModel>>(jsonString);
+                FileModel MovingItem = null;
+                foreach (var i in list)
+                {
+                    Console.WriteLine(i.TaskName);
+                    if (i.TaskName == model.TaskName)
+                    {
+                        i.TaskDescription = model.TaskDescription;
+                        i.Priority = model.Priority;
+                        MovingItem = i;
+                    }
+                }
+                list.Remove(MovingItem);
+                Console.WriteLine(model.Status);
+                if (model.Status == "Open Task")
+                {
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\new.json";
+                }
+                else if (model.Status == "In Progress")
+                {
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\inprogressdata.json";
+                }
+                else
+                {
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\finished.json";
+                }
+                var jsonString2 = System.IO.File.ReadAllText(jsonPath);
+                object jsonObject2 = JsonConvert.DeserializeObject(jsonString2);
+                var list2 = JsonConvert.DeserializeObject<List<FileModel>>(jsonString2);
+                list2.Add(MovingItem);
+                var convertedJson = JsonConvert.SerializeObject(list2, Formatting.Indented);
+                Console.WriteLine(convertedJson);
+                Console.WriteLine(jsonPath);
+                System.IO.File.WriteAllText(jsonPath, convertedJson);
+
+            }
+            else
+            {
+                if (model.Mode == "In Progress")
+                {
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\inprogressdata.json";
+                }
+                else if (model.Mode == "Open Task")
+                {
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\new.json";
+                }
+                else if (model.Mode == "Finished")
+                {
+                    jsonPath = currentDirectory + @"\wwwroot\ConfigData\finisheddata.json";
+
+                }
+
+                var jsonString = System.IO.File.ReadAllText(jsonPath);
+
+
+                object jsonObject = JsonConvert.DeserializeObject(jsonString);
+                var list = JsonConvert.DeserializeObject<List<FileModel>>(jsonString);
+                list.Add(model);
+                var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
+                Console.WriteLine(convertedJson);
+                Console.WriteLine(jsonPath);
+                System.IO.File.WriteAllText(jsonPath, convertedJson);
+            }
             return Ok();
         }
-        [HttpPut]
-        [Route("filesave3")]
-        public IActionResult PutFileNames3([FromBody] FileModel model)
-        {
-            Console.WriteLine("check");
-            string webRootPath = environment.WebRootPath;
-            string filesPath = Path.Combine(webRootPath, "files");
-            var currentDirectory = System.IO.Directory.GetCurrentDirectory();
-            string jsonPath = currentDirectory + @"\wwwroot\ConfigData\finisheddata.json";
-            string json = jsonPath;
-            var jsonString = System.IO.File.ReadAllText(json);
-            object jsonObject = JsonConvert.DeserializeObject(jsonString);
-            var list = JsonConvert.DeserializeObject<List<FileModel>>(jsonString);
-            list.Add(model);
-            var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
-            System.IO.File.WriteAllText(json, convertedJson);
-            return Ok();
-        }
+
 
         [HttpGet()]
         [Route("filedelete")]
         public String GetFileDelete(string file)
         {
+            Console.WriteLine(file);
             string[] fileProperties = file.Split('|');
 
             string json = JsonFilePath();
@@ -94,13 +143,13 @@ namespace backend.Controllers
                     deletedItem = listItem;
                 }
             }
-            Console.WriteLine("HJGJK  " + deletedItem);
+
             list.Remove(deletedItem);
             var convertedJson = JsonConvert.SerializeObject(list, Formatting.Indented);
             System.IO.File.WriteAllText(json, convertedJson);
             if (deletedItem == null)
             {
-                Console.WriteLine("null  " + deletedItem);
+
                 var currentDirectory = System.IO.Directory.GetCurrentDirectory();
                 string jsonPath = currentDirectory + @"\wwwroot\ConfigData\inprogressdata.json";
                 var jsonStrings = System.IO.File.ReadAllText(jsonPath);
@@ -109,23 +158,19 @@ namespace backend.Controllers
                 FileModel deletedItems = null;
                 foreach (var listItems in list1)
                 {
-                    Console.WriteLine(listItems.TaskName);
+
+
                     if (listItems.TaskName == fileProperties[0] && listItems.TaskDescription == fileProperties[1] && listItems.Priority == fileProperties[2])
                     {
                         deletedItems = listItems;
                     }
                 }
                 list1.Remove(deletedItems);
-                for (var i = 0; i < list1.Count; i++)
-                {
-                    Console.WriteLine(list1[i]);
-                }
+
                 var convertedJson2 = JsonConvert.SerializeObject(list1, Formatting.Indented);
                 System.IO.File.WriteAllText(jsonPath, convertedJson2);
                 if (deletedItems == null)
                 {
-
-                    Console.WriteLine("null  " + deletedItem);
                     var currentDirectory2 = System.IO.Directory.GetCurrentDirectory();
                     string jsonPath2 = currentDirectory2 + @"\wwwroot\ConfigData\finisheddata.json";
                     var jsonStrings2 = System.IO.File.ReadAllText(jsonPath2);
@@ -134,25 +179,20 @@ namespace backend.Controllers
                     FileModel deletedItem2 = null;
                     foreach (var listItems in list2)
                     {
-                        Console.WriteLine(listItems.TaskName);
+
+
                         if (listItems.TaskName == fileProperties[0] && listItems.TaskDescription == fileProperties[1] && listItems.Priority == fileProperties[2])
                         {
                             deletedItem2 = listItems;
                         }
                     }
                     list2.Remove(deletedItem2);
-                    for (var i = 0; i < list2.Count; i++)
-                    {
-                        Console.WriteLine(list2[i]);
-                    }
+
                     var convertedJson3 = JsonConvert.SerializeObject(list2, Formatting.Indented);
                     System.IO.File.WriteAllText(jsonPath2, convertedJson3);
 
                 }
             }
-
-
-
             return "File data deleted successfully";
         }
 
@@ -190,11 +230,8 @@ namespace backend.Controllers
         }
         public string JsonFilePath()
         {
-
-
             var currentDirectory = System.IO.Directory.GetCurrentDirectory();
-            string jsonPath = currentDirectory + @"\wwwroot\ConfigData\data.json";
-            Console.WriteLine(jsonPath);
+            string jsonPath = currentDirectory + @"\wwwroot\ConfigData\new.json";
             return jsonPath;
         }
 
--- backend/Models/FileModel.cs
+++ backend/Models/FileModel.cs
@@ -3,16 +3,21 @@ namespace backend.Models
 {
     public class FileModel
     {
-        public FileModel(string taskName, string taskDescription, string priority)
+        public FileModel(string taskName, string taskDescription, string priority, string mode, string status)
         {
             this.TaskName = taskName;
             this.TaskDescription = taskDescription;
             this.Priority = priority;
+            this.Mode = mode;
+            this.Status = status;
 
         }
         public string TaskName { get; set; }
         public string TaskDescription { get; set; }
         public string Priority { get; set; }
+        public string Mode { get; set; }
+
+        public string Status { get; set; }
 
     }
 }
